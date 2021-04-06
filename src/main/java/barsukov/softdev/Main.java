235340e16c848f29714107fe90482e718ff36fed
Nikita Barsukov <nikita@Nikitas-MacBook-Pro-2.local>
2021-04-06 18:50:47 +0300

    moved to plain JSONOnject

--- src/main/java/barsukov/softdev/Main.java
+++ src/main/java/barsukov/softdev/Main.java
@@ -1,11 +1,6 @@
 package barsukov.softdev;
 
 import barsukov.softdev.util.ProgressBar;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import model.LogOriginal;
-import model.LogTransformed;
 import org.json.JSONObject;
 
 import java.io.BufferedWriter;
@@ -24,33 +19,24 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 public class Main {
-    public static final String SCAN = "scan";
-    static private ObjectMapper mapper = new ObjectMapper()
-             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
-//    static private String f = "./phil.json";
-    static private String f = null;
-    static private String o = "./transformedLog.json";
-    static private String e = "./errors.json";
-    static private String k = "./logsKeys.json";
+    private static final String SCAN = "scan";
+    private static final String o    = "./transformedLog.json";
+    private static final String e    = "./errors.json";
+    private static final String k    = "./logsKeys.json";
+    private static       String f    = null;
 
-    static long processedBytes = 0L;
+    private static long processedBytes = 0L;
 
     public static void main(String[] args) throws IOException {
-        if (args.length == 2){
+        if (args.length == 2) {
             f = args[1];
         } else {
             System.out.println("Args is not set!");
             System.exit(1);
         }
 
-        try (
-            Stream<String> lines = Files.lines(Paths.get(f), Charset.defaultCharset());
-            RandomAccessFile oStream = new RandomAccessFile(o, "rw");
-            RandomAccessFile eStream = new RandomAccessFile(e, "rw");
-            FileChannel oChannel = oStream.getChannel();
-            FileChannel eChannel = eStream.getChannel();
-        ) {
-            if(SCAN.equals(args[0])) {
+        try (Stream<String> lines = Files.lines(Paths.get(f), Charset.defaultCharset()); RandomAccessFile oStream = new RandomAccessFile(o, "rw"); RandomAccessFile eStream = new RandomAccessFile(e, "rw"); FileChannel oChannel = oStream.getChannel(); FileChannel eChannel = eStream.getChannel();) {
+            if (SCAN.equals(args[0])) {
                 Set<String> commonKeySet = new HashSet<>();
                 lines.forEachOrdered(line -> scanLogKeys(line, commonKeySet));
                 writeKeysToFile(commonKeySet);
@@ -81,20 +67,20 @@ public class Main {
     }
 
     private static void processLog(String line, FileChannel oChannel, FileChannel eChannel, long totalSize) {
-        LogTransformed logtransformed = new LogTransformed();
+        JSONObject jsonLog       = new JSONObject(line);
+        JSONObject jsonLogTransf = new JSONObject();
+        jsonLog.keySet().forEach(key -> jsonLogTransf.put(key, transformValue(key, jsonLog)));
         try {
-            LogOriginal log = mapper.readValue(line, LogOriginal.class);
-            map(log, logtransformed);
-            byte[] strBytes = (mapper.writeValueAsString(logtransformed) + "\r\n").getBytes(StandardCharsets.UTF_8);
-            ByteBuffer buffer = ByteBuffer.allocate(strBytes.length);
+            byte[]     strBytes = (jsonLogTransf.toString() + "\r\n").getBytes(StandardCharsets.UTF_8);
+            ByteBuffer buffer   = ByteBuffer.allocate(strBytes.length);
             buffer.put(strBytes);
             buffer.flip();
             oChannel.write(buffer);
             processedBytes += strBytes.length;
         } catch (IOException e) {
             try {
-                byte[] strBytes = (line + "\r\n").getBytes(StandardCharsets.UTF_8);
-                ByteBuffer buffer = ByteBuffer.allocate(strBytes.length);
+                byte[]     strBytes = (line + "\r\n").getBytes(StandardCharsets.UTF_8);
+                ByteBuffer buffer   = ByteBuffer.allocate(strBytes.length);
                 buffer.put(strBytes);
                 buffer.flip();
                 eChannel.write(buffer);
@@ -106,23 +92,7 @@ public class Main {
         ProgressBar.updateProgressBar(processedBytes, totalSize);
     }
 
-    private static void map(LogOriginal log, LogTransformed transformedLog) {
-        transformedLog.setUsername(log.getUsername());
-        transformedLog.setEvent_type(log.getEvent_type());
-        transformedLog.setIp(log.getIp());
-        transformedLog.setAgent(log.getAgent());
-        transformedLog.setHost(log.getHost());
-        transformedLog.setReferer(log.getReferer());
-        transformedLog.setAccept_language(log.getAccept_language());
-        transformedLog.setEvent(log.getEvent());
-        transformedLog.setEvent_source(log.getEvent_source());
-        transformedLog.setTime(log.getTime());
-        transformedLog.setPage(log.getPage());
-
-        try {
-            transformedLog.setContext(mapper.writeValueAsString(log.getContext()));
-        } catch (JsonProcessingException jsonProcessingException) {
-            jsonProcessingException.printStackTrace();
-        }
+    private static String transformValue(String key, JSONObject jsonLog) {
+        return jsonLog.get(key).toString();
     }
 }
