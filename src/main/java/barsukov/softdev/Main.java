ab339946b27be995e07e13cef7743a4b1ec7a711
Nikita Barsukov <nikita@Nikitas-MacBook-Pro-2.local>
2021-04-06 16:49:46 +0300

    added: scan log keys functionality; fix annotation;

--- src/main/java/barsukov/softdev/Main.java
+++ src/main/java/barsukov/softdev/Main.java
@@ -6,8 +6,11 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import model.LogOriginal;
 import model.LogTransformed;
+import org.json.JSONObject;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
@@ -16,22 +19,27 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public class Main {
-    static ObjectMapper mapper = new ObjectMapper()
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
-    static String f = null;
-    static String o = "./transformedLog.json";
-    static String e = "./errors.json";
+    public static final String SCAN = "scan";
+    static private ObjectMapper mapper = new ObjectMapper()
+             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+//    static private String f = "./phil.json";
+    static private String f = null;
+    static private String o = "./transformedLog.json";
+    static private String e = "./errors.json";
+    static private String k = "./logsKeys.json";
 
     static long processedBytes = 0L;
 
     public static void main(String[] args) throws IOException {
-        if (args.length != 0){
-            f = args[0];
+        if (args.length == 2){
+            f = args[1];
         } else {
-            System.out.println("File is not set!");
+            System.out.println("Args is not set!");
             System.exit(1);
         }
 
@@ -42,11 +50,37 @@ public class Main {
             FileChannel oChannel = oStream.getChannel();
             FileChannel eChannel = eStream.getChannel();
         ) {
-            lines.forEachOrdered(line -> transformLog(line, oChannel, eChannel, new File(f).length()));
+            if(SCAN.equals(args[0])) {
+                Set<String> commonKeySet = new HashSet<>();
+                lines.forEachOrdered(line -> scanLogKeys(line, commonKeySet));
+                writeKeysToFile(commonKeySet);
+            } else {
+                lines.forEachOrdered(line -> processLog(line, oChannel, eChannel, new File(f).length()));
+            }
         }
     }
 
-    private static void transformLog(String line, FileChannel oChannel, FileChannel eChannel, long totalSize) {
+    private static void writeKeysToFile(Set<String> commonKeySet) {
+        File file = new File(k);
+        try (BufferedWriter bf = new BufferedWriter(new FileWriter(file))) {
+            for (String key : commonKeySet) {
+                bf.write(key);
+                bf.newLine();
+            }
+            bf.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void scanLogKeys(String line, Set<String> commonKeySet) {
+        JSONObject jsonLog = new JSONObject(line);
+        commonKeySet.addAll(jsonLog.keySet());
+        processedBytes += line.getBytes().length;
+        ProgressBar.updateProgressBar(processedBytes, new File(f).length());
+    }
+
+    private static void processLog(String line, FileChannel oChannel, FileChannel eChannel, long totalSize) {
         LogTransformed logtransformed = new LogTransformed();
         try {
             LogOriginal log = mapper.readValue(line, LogOriginal.class);
@@ -69,9 +103,7 @@ public class Main {
                 ioException.printStackTrace();
             }
         }
-
         ProgressBar.updateProgressBar(processedBytes, totalSize);
-
     }
 
     private static void map(LogOriginal log, LogTransformed transformedLog) {
